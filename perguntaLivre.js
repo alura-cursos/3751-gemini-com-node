8ef5105cfcba08249f5b773e7d24cc6b89cf657a
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-04-23 17:03:46 -0300

    vídeo 4.3

--- perguntaLivre.js
+++ perguntaLivre.js
@@ -12,11 +12,20 @@ export async function perguntar() {
     {text: "output: "},
   ];
 
-  
-  const result = await model.generateContent(
+  const requisicao = (
     {contents: [{ role: "user", parts }]}
   );
+  
+  const result = await model.generateContent(requisicao);
+  
+  const totalTokensEntrada = await model.countTokens(requisicao);
+  console.log(`\nTotal tokens de entrada:  ${totalTokensEntrada.totalTokens}\n`)
+  
   const response = await result.response;
   const text = response.text();
   console.log(text);
+
+  const totalTokensSaida = await model.countTokens(text);
+  console.log(`\nTotal tokens de saída:  ${totalTokensSaida.totalTokens}\n`)
+  
 }
